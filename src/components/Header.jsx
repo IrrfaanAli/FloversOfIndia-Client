8c6e4c539535c31e1868f15c21f4694b76063515
Irfan Ali <alimohammadirfan7@gmail.com>
2023-05-02 09:28:28 +0600

    navbar added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,12 +14,11 @@ const Header = () => {
                 <div className="form-control  mx-10">
                         <button className='text-xl font-semibold'>Blog</button>
                 </div>
-                
-                   
+  
                     <div className="dropdown dropdown-end">
                         <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
                             <div className="w-10 rounded-full">
-                                <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                                <img src="" />
                             </div>
                         </label>
                         
